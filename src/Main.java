d66e7049069b4849a708b5242f87ba1abef940c7
Emrullah Tanıma <emrtnm@gmail.com>
2023-09-01 21:05:05 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    @BeforeSuite
+    @Parameters("webDriver")
+    public void init(String webDriver) {
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.SEVERE);
+
+        switch (webDriver)
+        {
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "ie":
+                driver = new InternetExplorerDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+            default:
+                driver = new ChromeDriver();
+                break;
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    @Test(priority = 1)
+    public void US1() {
+
+    }
+
+    @AfterSuite
+    public void close() {
+        driver.quit();
+    }
+}
