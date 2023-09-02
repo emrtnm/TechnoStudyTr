018d60822b0a2a2b9d7437cb8b95243539e113ab
Emrullah Tanıma <emrtnm@gmail.com>
2023-09-02 11:09:56 +0300

    Added coding env

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 import java.util.logging.Level;
@@ -17,7 +15,9 @@ import java.util.logging.Logger;
 public class Main {
     public WebDriver driver;
     public WebDriverWait wait;
-    @BeforeSuite
+    public String baseUrl = "https://techno.study/tr/";
+
+    @BeforeClass
     @Parameters("webDriver")
     public void init(String webDriver) {
         Logger logger = Logger.getLogger("");
@@ -27,6 +27,9 @@ public class Main {
         {
             case "firefox":
                 driver = new FirefoxDriver();
+                break;
+            case "safari":
+                driver = new SafariDriver();
                 break;
             case "ie":
                 driver = new InternetExplorerDriver();
@@ -45,12 +48,69 @@ public class Main {
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
-    @Test(priority = 1)
-    public void US1() {
+    @BeforeMethod
+    void openUri()
+    {
+        driver.get(baseUrl);
+    }
+
+    // @assigned=Samet Çamoğlu
+    @Test
+    void US1CoursesMenu()
+    {
 
     }
 
-    @AfterSuite
+    // @assigned=Samet Çamoğlu
+    @Test
+    void US2CampusLogin()
+    {
+
+    }
+
+    // @assigned=Rustam Rozbayev
+    @Test
+    void US3ApplyToCourse()
+    {
+
+    }
+
+    // @assigned=Selen Dilek
+    @Test
+    void US4FooterCoursesMenu()
+    {
+
+    }
+
+    // @assigned=Selen Dilek
+    @Test
+    void US5AccessSocialsMedia()
+    {
+
+    }
+
+    // @assigned=Emrullah Tanima
+    @AfterMethod
+    void US6CheckLogoRedirect()
+    {
+
+    }
+
+    // @assigned=Ümit Boyraz
+    @Test
+    void US7CourseDetails()
+    {
+
+    }
+
+    // @assigned=Umut Can Güzel
+    @Test
+    void US8AcceptTermsOfUse()
+    {
+
+    }
+
+    @AfterClass
     public void close() {
         driver.quit();
     }
