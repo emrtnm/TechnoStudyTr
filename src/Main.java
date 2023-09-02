7684ee8db48cb4bf79bb4e603a5308d35a249391
Selen Dilek <yildirimselen17@gmail.com>
2023-09-02 11:01:24 -0400

    First commit from selen branch.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -76,16 +83,114 @@ public class Main {
     }
 
     // @assigned=Selen Dilek
+    //Access to courses from the lower menu
     @Test
-    void US4FooterCoursesMenu()
-    {
+    void US4FooterCoursesMenu() throws InterruptedException {
+        //BUG 1 : Kurslarin adi var ama kisa adi yoktur. Sadece Yazilim Test Muhendisligi icin kisa ad var "SDET"
+
+        WebElement courses =driver.findElement(By.xpath("//a[@data-tooltip-menu-id='516093139']"));
+        Assert.assertTrue(courses.isDisplayed());
+        new Actions(driver).moveToElement(courses).build().perform();
+
+        List<WebElement> listofCourses=driver.findElements(By.xpath("//div[@class='t966__menu-item-title t-name']"));
+        for(WebElement e : listofCourses){
+            System.out.println("e.getText() = " + e.getText());
+
+
+            wait.until(ExpectedConditions.visibilityOf(e));
+            Assert.assertTrue(e.isDisplayed());
+
+        }
+
+        for(WebElement e : listofCourses){
+
+            Assert.assertTrue(e.isEnabled());
+
+        }
+
+        for(WebElement e : listofCourses){
+
+           e.click();
+           Thread.sleep(1000);
+           ////a[@class='tn-atom']
+            WebElement detayliBilgi =
+                    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='tn-atom'])[1]")));
+
+            Assert.assertTrue(detayliBilgi.isEnabled()); //Not Veri Bilimi Bootcamp indeki detaylibilgi butonu tiklanmiyor.(BUG2)
+            detayliBilgi.click();
+
+            //BUG(3)
+            //Yazilim Test muhendisligi icin detayli bilgiye tiklayinca farkli bir alana yonlendiriyor digerlerinden farkli bir alana
+
+
+
+
+        }
+
+        driver.navigate().refresh();
+
+        WebElement courses2 =driver.findElement(By.cssSelector("[class='t-menu__link-item t966__tm-link']"));
+        List<WebElement> listofCourses2=driver.findElements(By.cssSelector("[class='t966__menu-item-title t-name']"));
+
+        for(WebElement course : listofCourses2){
+            new Actions(driver).moveToElement(courses2).build().perform();
+            wait.until(ExpectedConditions.visibilityOf(course));
+            course.click();
+
+            WebElement element =
+                    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='tn-atom'])[1]")));
+            String title = driver.getTitle();
+            System.out.println("title = " + title);
+
+            Assert.assertTrue(element.getText().equalsIgnoreCase(title));
+            //Not : yazilim test muhendisinin kisa adi SDET olarak yazilmis digerlerinde kisa adi yok. O yuzden hata aliyorum
+            //element.getText()=Yazılım Test Mühendisi
+            //title=SDET Yazılım Test Mühendisi (BUG4)
+
+            //BUG5  () : Master's Degree tiklayinca beni turkce sayfadan ingilizce sayfaya yonlendiriyor .
+
+            driver.navigate().back();
+
+            //BUG6 : Kurslar bolumune mouseover yaptigim zaman JobCenter & Arbeissamt kursuna tikladigimda sayfanin basliginda
+            //kursun adi gozukmuyor ve sol ust kosede yer alan kurslar bolumu gozukmuyor.
+
+
+        }
 
     }
 
     // @assigned=Selen Dilek
+    //Access to social media accounts from the sub -menu
     @Test
     void US5AccessSocialsMedia()
-    {
+
+    {   String homePage=driver.getWindowHandle();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        List<WebElement> linkler = driver.findElements(By.xpath("//a[@rel='nofollow noopener']"));
+        for(WebElement link : linkler){
+
+            Assert.assertTrue(link.isDisplayed()); //linkler goruluyor mu
+            Assert.assertTrue(link.isEnabled()); //linkler tiklanabiliyor mu
+            //linklere tiklayinca sayfala aciliyor mu
+            js.executeScript("arguments[0].scrollIntoView(true);", link);
+            js.executeScript("arguments[0].click();", link);
+
+            Set<String> sosyalMedyaSayfalari = driver.getWindowHandles();
+            for(String idS : sosyalMedyaSayfalari){
+
+                Assert.assertTrue(driver.getCurrentUrl().toLowerCase().contains("techno"));
+
+
+            }
+
+           
+           
+        }
+
+
+
+
+
 
     }
 
@@ -110,8 +215,8 @@ public class Main {
 
     }
 
-    @AfterClass
-    public void close() {
-        driver.quit();
-    }
+//    @AfterClass
+//    public void close() {
+//        driver.quit();
+//    }
 }
